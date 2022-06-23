e0dc638c45a9ecbcbf2297811049edfe295f7b52
Heiko Klare <dev@heiko.klare.info>
2022-06-23 11:40:37 +0200

    Pass change to be propagated to change propagation listener

--- bundles/tools.vitruv.change.composite/src/tools/vitruv/change/composite/propagation/ChangePropagationListener.java
+++ bundles/tools.vitruv.change.composite/src/tools/vitruv/change/composite/propagation/ChangePropagationListener.java
@@ -1,6 +1,7 @@
 package tools.vitruv.change.composite.propagation;
 
 import tools.vitruv.change.composite.description.PropagatedChange;
+import tools.vitruv.change.composite.description.VitruviusChange;
 
 /**
  * Listener for the change propagation status.
@@ -9,13 +10,15 @@ public interface ChangePropagationListener {
 
 	/**
 	 * Called before the change propagation is started.
+	 * 
+	 * @param changeToPropagate the change to be propagated, must not be <code>null</code>
 	 */
-	void startedChangePropagation();
+	void startedChangePropagation(VitruviusChange changeToPropagate);
 
 	/**
 	 * Called after the change propagation is finished.
 	 * 
-	 * @param propagatedChanges the changes that have generated during propagation
+	 * @param propagatedChanges the changes that have been generated during propagation, must not be <code>null</code>
 	 */
 	void finishedChangePropagation(Iterable<PropagatedChange> propagatedChanges);
 
