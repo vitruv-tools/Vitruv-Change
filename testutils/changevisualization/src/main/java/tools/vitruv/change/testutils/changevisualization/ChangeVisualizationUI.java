1e5472b1b2095d09235a53895d959197a3b5e7db
Muhammad-Tahir-KIT <muhammad.tahir@kit.edu>
2025-04-10 12:45:44 +0200

    Update ChangeVisualizationUI.java

--- testutils/changevisualization/src/main/java/tools/vitruv/change/testutils/changevisualization/ChangeVisualizationUI.java
+++ testutils/changevisualization/src/main/java/tools/vitruv/change/testutils/changevisualization/ChangeVisualizationUI.java
@@ -99,6 +99,9 @@ public class ChangeVisualizationUI extends JFrame implements MonitoredRepository
   /** Default font used for menuItems . */
   public static final Font DEFAULT_MENUITEM_FONT = createFont("MenuItem.font", 16, Font.PLAIN);
 
+  // Define a constant in your class to make it more descriptive
+  private static final int CLOSE_OPERATION = WindowConstants.DISPOSE_ON_CLOSE;
+
   private final ChangeVisualizationDataModel changeVisualizationDataModel;
 
   private JTabbedPane tabbedPane;
@@ -124,7 +127,7 @@ public class ChangeVisualizationUI extends JFrame implements MonitoredRepository
   }
 
   private void initializeWindow() {
-    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    setDefaultCloseOperation(CLOSE_OPERATION);
     int screenWidth = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
     int screenHeight = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
     setSize(Math.min(screenWidth - 30, 1890), Math.min(screenHeight - 60, 1020));
