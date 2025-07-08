67e51decdf256e23487f8d5715812c4679ea969b
tsotnemikadze <tsotnemikadze@gmail.com>
2025-07-08 17:40:25 +0200

    Make this anonymous inner class a lambda

--- testutils/changevisualization/src/main/java/tools/vitruv/change/testutils/changevisualization/ui/SelectionDialog.java
+++ testutils/changevisualization/src/main/java/tools/vitruv/change/testutils/changevisualization/ui/SelectionDialog.java
@@ -24,16 +24,19 @@ public class SelectionDialog extends JDialog {
   private JCheckBox[] boxes;
 
   /**
-   * When closing via the done-button this value is set to true. This way a valid exit can be
+   * When closing via the done-button this value is set to true. This way a valid
+   * exit can be
    * distinguished from just closing the dialog or other cancel-behavior.
    */
   private boolean validClose = false;
 
   /**
-   * Create a dialog that displays the given strings for selection. All are selected by default.
+   * Create a dialog that displays the given strings for selection. All are
+   * selected by default.
    *
-   * @param frame Used to position the dialog on screen and to return the input-focus to after
-   *     closing.
+   * @param frame  Used to position the dialog on screen and to return the
+   *               input-focus to after
+   *               closing.
    * @param values The values presented to the user for selection
    */
   public SelectionDialog(JFrame frame, String[] values) {
@@ -43,13 +46,17 @@ public class SelectionDialog extends JDialog {
   /**
    * Create a dialog that displays the given strings for selection.
    *
-   * <p>The initial selection state is given in die initialSelections array. It this array is null,
+   * <p>
+   * The initial selection state is given in die initialSelections array. It this
+   * array is null,
    * all are selected by default.
    *
-   * @param frame Used to position the dialog on screen and to return the input-focus to after
-   *     closing.
-   * @param values The values presented to the user for selection
-   * @param initialSelections The initial selection states. Either null or of same size as values.
+   * @param frame             Used to position the dialog on screen and to return
+   *                          the input-focus to after
+   *                          closing.
+   * @param values            The values presented to the user for selection
+   * @param initialSelections The initial selection states. Either null or of same
+   *                          size as values.
    */
   public SelectionDialog(JFrame frame, String[] values, boolean[] initialSelections) {
     super(frame, true);
@@ -62,7 +69,7 @@ public class SelectionDialog extends JDialog {
   /**
    * Creates the ui.
    *
-   * @param values The values displayed for selection
+   * @param values            The values displayed for selection
    * @param initialSelections Initial selection state, may be null
    */
   private void createUi(String[] values, boolean[] initialSelections) {
@@ -79,20 +86,18 @@ public class SelectionDialog extends JDialog {
     // If closed via the done button, the selection is marked as valid
     JButton done = new JButton("Done");
     done.setFont(ChangeVisualizationUI.DEFAULT_BUTTON_FONT);
-    done.addActionListener(
-        new ActionListener() {
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            validClose = true;
-            setVisible(false);
-          }
-        });
+    done.addActionListener(e -> {
+      validClose = true;
+      setVisible(false);
+    });
     add(done);
   }
 
   /**
-   * Return an array of booleans holding the final selection state of the user. If the user
-   * cancelled the dialog (not exited via "done" button), nothing is marked as selected, even if the
+   * Return an array of booleans holding the final selection state of the user. If
+   * the user
+   * cancelled the dialog (not exited via "done" button), nothing is marked as
+   * selected, even if the
    * user had selected something prior to canceling the dialog.
    *
    * @return The selection state of the values displayed
