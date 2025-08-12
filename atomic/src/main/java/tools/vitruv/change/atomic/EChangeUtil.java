63ce22245a14e0fecc032c0e3add2321646f0d66
Benedikt Jutz <benedikt.jutz@kit.edu>
2025-08-12 17:38:36 +0200

    Satisfy Checkstyle, SonarQube

--- atomic/src/main/java/tools/vitruv/change/atomic/EChangeUtil.java
+++ atomic/src/main/java/tools/vitruv/change/atomic/EChangeUtil.java
@@ -12,27 +12,59 @@ import tools.vitruv.change.atomic.root.RemoveRootEObject;
  */
 @Utility
 public class EChangeUtil {
-	/**
-	 * Private constructor for utility class.
-	 */
-	private EChangeUtil() {
-	}
+  /**
+   * Private constructor for utility class.
+   */
+  private EChangeUtil() {
+  }
 
-	public static boolean isContainmentRemoval(final EChange<?> change) {
-		if (change instanceof ReplaceSingleValuedEReference<?> replaceReference) {
-			return replaceReference.getAffectedFeature().isContainment() &&
-				((replaceReference.getOldValue() != null && replaceReference.getOldValue() != replaceReference.getNewValue()));
-		}
-		if (change instanceof RemoveEReference<?> removeReference) {
-			return removeReference.getAffectedFeature().isContainment();
-		}
-		return change instanceof RemoveRootEObject;
-	}
+  /**
+   * Tests if change represents the removal of an {@link EObject} from the containment hierarchy.
+   * 
+   * <p>This is the case when:
+   * <ol>
+   *  <li>
+   *    change replaces an EObject in a single-valued containment reference, in which case
+   *    that EObject is considered to be removed (ReplaceSingleValuedEReference),
+   *  </li>
+   *  <li>change removes an EObject from a containment reference (RemoveEReference), or</li>,
+   *  <li>change removes a root object (RemoveRootEObject)</li>
+   * </ol>
+   *
+   * @param change - {@link EChange}
+   * @return a boolean value.
+   */
+  public static boolean isContainmentRemoval(final EChange<?> change) {
+    if (change instanceof ReplaceSingleValuedEReference<?> replaceReference) {
+      return replaceReference.getAffectedFeature().isContainment() 
+          && (replaceReference.getOldValue() != null 
+          && replaceReference.getOldValue() != replaceReference.getNewValue());
+    }
+    if (change instanceof RemoveEReference<?> removeReference) {
+      return removeReference.getAffectedFeature().isContainment();
+    }
+    return change instanceof RemoveRootEObject;
+  }
 
-	public static boolean isContainmentInsertion(final EChange<?> change) {
-		if (change instanceof AdditiveReferenceEChange<?> additiveReferenceChange) {
-			return additiveReferenceChange.getAffectedFeature().isContainment() && additiveReferenceChange.getNewValue() != null;  
-		}
-		return change instanceof RemoveRootEObject || change instanceof InsertRootEObject;
-	}
+  /**
+   * Tests if change represents the insertion of an {@link EObject} into the containment hierarchy.
+   * 
+   * <p>This is the case when:
+   * <ol>
+   *  <li>
+   *    change adds an EObject into a containment reference (AdditiveReferenceEChange), or:
+   *  </li>
+   *  <li>change inserts or removes a root object (InsertRootEObject/RemoveRootEObject).</li>
+   * </ol>
+   *
+   * @param change - {@link EChange}
+   * @return a boolean value.
+   */
+  public static boolean isContainmentInsertion(final EChange<?> change) {
+    if (change instanceof AdditiveReferenceEChange<?> additiveReferenceChange) {
+      return additiveReferenceChange.getAffectedFeature().isContainment()
+          && additiveReferenceChange.getNewValue() != null;  
+    }
+    return change instanceof RemoveRootEObject || change instanceof InsertRootEObject;
+  }
 }
