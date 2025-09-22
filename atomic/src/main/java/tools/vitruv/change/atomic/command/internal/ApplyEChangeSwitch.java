f8bf137549085c2a3267c8145f0ee3662e3b4bdf
Benedikt Jutz <benedikt.jutz@kit.edu>
2025-09-22 14:29:15 +0200

    turn ApplyEChangeSwitch observable

--- atomic/src/main/java/tools/vitruv/change/atomic/command/internal/ApplyEChangeSwitch.java
+++ atomic/src/main/java/tools/vitruv/change/atomic/command/internal/ApplyEChangeSwitch.java
@@ -3,15 +3,19 @@ package tools.vitruv.change.atomic.command.internal;
 import static com.google.common.base.Preconditions.checkState;
 
 import edu.kit.ipd.sdq.activextendannotations.Utility;
+import java.util.LinkedList;
+import java.util.List;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.ecore.EObject;
 import tools.vitruv.change.atomic.EChange;
+import tools.vitruv.change.atomic.command.ApplyEChangeObserver;
 
 /**
  * Utility class for applying an EChange.
  */
 @Utility
 public class ApplyEChangeSwitch {
+  private static final List<ApplyEChangeObserver> observers = new LinkedList<>();
   /**
    * Forward switch to apply {@link EChange}s.
    */
@@ -27,6 +31,24 @@ public class ApplyEChangeSwitch {
   private ApplyEChangeSwitch() {}
 
   /**
+   * Registers the observer to observe the start and end of {@link #applyEChange}.
+   *
+   * @param observer - {@link ApplyEChangeObserver}
+   */
+  public static void registerObserver(ApplyEChangeObserver observer) {
+    observers.add(observer);
+  }
+
+  /**
+   * Deregisters observer.
+   *
+   * @param observer - {@link ApplyEChangeObserver}
+   */
+  public static void deregisterObserver(ApplyEChangeObserver observer) {
+    observers.remove(observer);
+  }
+
+  /**
    * Applies a given {@link EChange}.
    * Returns if the change was successfully applied.
    *
@@ -36,6 +58,7 @@ public class ApplyEChangeSwitch {
    *      or they cannot be executed.
    */
   public static void applyEChange(EChange<EObject> change, boolean applyForward) {
+    observers.forEach(applyObserver -> applyObserver.startToApplyEChange(change, applyForward));
     final var commandSwitch = applyForward ? forwardSwitch : backwardSwitch;
     var commands = commandSwitch.getCommands(change);
     checkState(commands != null, "no commands could be generated for EChange: %s", change);
@@ -44,5 +67,6 @@ public class ApplyEChangeSwitch {
       checkState(c.canExecute(), "cannot execute command generated for EChange: %s", change);
       c.execute();
     }
+    observers.forEach(applyObserver -> applyObserver.endApplyEChange(commands));
   }
 }
