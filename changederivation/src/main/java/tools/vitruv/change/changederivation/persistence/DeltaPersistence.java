aac7071f32f0869d3c433205831aeb8071045b8a
Martin Armbruster <armbrustermartin@googlemail.com>
2025-12-08 09:40:51 +0100

    Corrected the file name for the DeltaPersistence class, and added documentation for it.

--- changederivation/src/main/java/tools/vitruv/change/changederivation/persistence/DeltaPersistence.java
+++ changederivation/src/main/java/tools/vitruv/change/changederivation/persistence/DeltaPersistence.java
@@ -18,7 +18,21 @@ import tools.vitruv.change.atomic.EChange;
 import tools.vitruv.change.atomic.hid.HierarchicalId;
 import tools.vitruv.change.changederivation.DefaultStateBasedChangeResolutionStrategy;
 
-public class DeltaPersistance {
+/**
+ * A utility class, which supports in the persistence of deltas / changes of models.
+ */
+public final class DeltaPersistence {
+    private DeltaPersistence() {}
+
+    /**
+     * Saves a model in an EMF {@see Resource} in the form of atomic {@see EChange}s in a given file.
+     * When loaded and applied, the changes simulate the creation of the given model
+     * and result in the same model state as given in the EMF Resource.
+     * 
+     * @param resource the EMF Resource with the model to store.
+     * @param storage path to the file, in which the changes are stored.
+     * @throws IOException if the changes cannot be stored.
+     */
     public static void saveResourceAsChanges(Resource resource, Path storage) throws IOException {
         List<EChange<HierarchicalId>> changes = new DefaultStateBasedChangeResolutionStrategy()
             .getChangeSequenceForCreated(resource)
@@ -26,6 +40,13 @@ public class DeltaPersistance {
         saveChanges(changes, storage);
     }
 
+    /**
+     * Saves a list of {@see EChange}s in a file.
+     * 
+     * @param changes the list of changes to store.
+     * @param deltaFile path to the file, in which the changes are stored.
+     * @throws IOException if the changes cannot be stored.
+     */
     public static void saveChanges(List<EChange<HierarchicalId>> changes, Path deltaFile) throws IOException {
         ResourceSet resourceSet = withGlobalFactories(new ResourceSetImpl());
 
