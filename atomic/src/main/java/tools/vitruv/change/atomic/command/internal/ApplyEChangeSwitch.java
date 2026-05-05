45e617bb48b228cf637b615d1d405d7ad07b3747
Benedikt Jutz <benedikt.jutz@kit.edu>
2026-05-05 16:06:55 +0200

    Uuid handling changes

--- atomic/src/main/java/tools/vitruv/change/atomic/command/internal/ApplyEChangeSwitch.java
+++ atomic/src/main/java/tools/vitruv/change/atomic/command/internal/ApplyEChangeSwitch.java
@@ -51,22 +51,34 @@ public class ApplyEChangeSwitch {
    * Applies a given {@link EChange}.
    * Returns if the change was successfully applied.
    *
-   * @param change       - The {@link EChange} which will be applied.
-   * @param applyForward - If {@code true} the change will be applied forward,
-   *                     otherwise backward.
-   * @throws IllegalStateException - No commands can be generated for the change,
-   *                               or they cannot be executed.
+   * @param change - The {@link EChange} which will be applied.
+   * @param applyForward - If {@code true} the change will be applied forward, otherwise backward.
+   * @throws IllegalStateException - No commands can be generated for the change, 
+   *      or they cannot be executed.
    */
   public static void applyEChange(EChange<EObject> change, boolean applyForward) {
     observers.forEach(applyObserver -> applyObserver.startToApplyEChange(change, applyForward));
-    final var commandSwitch = applyForward ? forwardSwitch : backwardSwitch;
-    var commands = commandSwitch.getCommands(change);
-    checkState(commands != null, "no commands could be generated for EChange: %s", change);
+    var commands = getCommands(change, applyForward);
 
     for (Command c : commands) {
       checkState(c.canExecute(), "cannot execute command generated for EChange: %s", change);
       c.execute();
     }
     observers.forEach(applyObserver -> applyObserver.endApplyEChange(commands));
+  }
+
+  /**
+   * Computes the EMF {@link Command}s required to apply {@code change} in the given direction
+   * (forward or backward).
+   *
+   * @param change - {@link EChange}
+   * @param applyForward - boolean
+   * @return {@link List} of {@link EChange}
+   */
+  public static List<Command> getCommands(EChange<EObject> change, boolean applyForward) {
+    final var commandSwitch = applyForward ? forwardSwitch : backwardSwitch;
+    var commands = commandSwitch.getCommands(change);
+    checkState(commands != null, "no commands could be generated for EChange: %s", change);
+    return commands;
   }
 }
