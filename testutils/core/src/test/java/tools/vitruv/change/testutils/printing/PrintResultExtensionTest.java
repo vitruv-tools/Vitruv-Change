ea6ef63df0dde04d4bdd580ebda3d914fc583e65
OAHstasy <o.anilhicyilmaz@gmail.com>
2026-07-08 16:40:40 +0200

    refactor checkstyle issue modification

--- testutils/core/src/test/java/tools/vitruv/change/testutils/printing/PrintResultExtensionTest.java
+++ testutils/core/src/test/java/tools/vitruv/change/testutils/printing/PrintResultExtensionTest.java
@@ -12,25 +12,25 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class PrintResultExtensionTest {
     @ParameterizedTest
     @CsvSource({
-            "PRINTED, PRINTED, PRINTED",
-            "PRINTED, PRINTED_NO_OUTPUT, PRINTED",
-            "PRINTED_NO_OUTPUT, PRINTED, PRINTED",
-            "PRINTED_NO_OUTPUT, PRINTED_NO_OUTPUT, PRINTED_NO_OUTPUT",
-            "PRINTED_NO_OUTPUT, NOT_RESPONSIBLE, NOT_RESPONSIBLE",
-            "NOT_RESPONSIBLE, PRINTED_NO_OUTPUT, NOT_RESPONSIBLE",
-            "NOT_RESPONSIBLE, NOT_RESPONSIBLE, NOT_RESPONSIBLE"
+        "PRINTED, PRINTED, PRINTED",
+        "PRINTED, PRINTED_NO_OUTPUT, PRINTED",
+        "PRINTED_NO_OUTPUT, PRINTED, PRINTED",
+        "PRINTED_NO_OUTPUT, PRINTED_NO_OUTPUT, PRINTED_NO_OUTPUT",
+        "PRINTED_NO_OUTPUT, NOT_RESPONSIBLE, NOT_RESPONSIBLE",
+        "NOT_RESPONSIBLE, PRINTED_NO_OUTPUT, NOT_RESPONSIBLE",
+        "NOT_RESPONSIBLE, NOT_RESPONSIBLE, NOT_RESPONSIBLE"
     })
     void combinesPrintResults(
             final PrintResult previous,
             final PrintResult latest,
             final PrintResult expected) {
                 assertEquals(expected, PrintResultExtension.operatorPlus(previous, latest));
-            }
+    }
 
     @ParameterizedTest
     @CsvSource({
-            "PRINTED, NOT_RESPONSIBLE",
-            "NOT_RESPONSIBLE, PRINTED"
+        "PRINTED, NOT_RESPONSIBLE",
+        "NOT_RESPONSIBLE, PRINTED"
     })
     void rejectsInvalidPrintResultTransitions(
             final PrintResult previous,
